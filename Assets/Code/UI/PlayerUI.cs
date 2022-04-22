91d47f5204c5ed095b58d98794e23544f08940a9
Dave <davy.van.verdegem@student.howest.be>
2022-04-22 10:46:49 +0200

    * Disabled bonus cow text.

--- Assets/Code/UI/PlayerUI.cs
+++ Assets/Code/UI/PlayerUI.cs
@@ -38,8 +38,8 @@ public class PlayerUI : MonoBehaviour
 
 		_player = player;
 		UpdateScore(_player);
-		_bonusCowDescriptionText.text = _player.BonusCow.Description;
-		_bonusCowDescriptionText.enabled = true;
+		//_bonusCowDescriptionText.text = _player.BonusCow.Description;
+		//_bonusCowDescriptionText.enabled = true;
 
 		_bonusCowImage.sprite = _player.BonusCow.Sprite;
 		_bonusCowImage.enabled = true;
